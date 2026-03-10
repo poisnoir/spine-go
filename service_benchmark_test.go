54a0fca6598c7b30bd372836f6981e700b49a990
agharibipour <amir.gharibipour@ubisoft.com>
2026-03-10 15:22:18 -0400

    added benchmark

---
+++ service_benchmark_test.go
@@ -0,0 +1,72 @@
+package spine
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"testing"
+)
+
+func BenchmarkServiceCall(b *testing.B) {
+	logger := slog.New(slog.NewJSONHandler(io.Discard, nil))
+	ns, err := JointNamespace("bench", "secret", logger, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	handler := func(input uint32) (uint32, error) {
+		return input * 2, nil
+	}
+
+	_, err = NewService(ns, "math", handler)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	caller, err := NewServiceCaller[uint32, uint32](ns, "math")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := caller.Call(uint32(i), context.Background())
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkServiceCallParallel(b *testing.B) {
+	logger := slog.New(slog.NewJSONHandler(io.Discard, nil))
+	ns, err := JointNamespace("bench_parallel", "secret", logger, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	handler := func(input uint32) (uint32, error) {
+		return input * 2, nil
+	}
+
+	_, err = NewService(ns, "math_parallel", handler)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	caller, err := NewServiceCaller[uint32, uint32](ns, "math_parallel")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		i := uint32(0)
+		for pb.Next() {
+			_, err := caller.Call(i, context.Background())
+			if err != nil {
+				b.Fatal(err)
+			}
+			i++
+		}
+	})
+}
