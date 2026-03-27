94b9f73f0c6ab7b64a4bfb7d66b4e23083e34e1e
agharibipour <amir.gharibipour@ubisoft.com>
2026-03-27 13:48:04 -0400

    fix: removed py files

--- py/__init__.py
+++
@@ -1,5 +0,0 @@
-from .namespace import Namespace
-from .service import Service, new_service
-from .service_caller import ServiceCaller
-from .threaded_service import ThreadedService, new_threaded_service
-from .mad import Mad, new_mad
